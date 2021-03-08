f5739af4ea1ca9aec72c79bba2d182827d6e853f
sanya <sanya923@mail.ru>
2021-03-09 01:04:06 +0300

    react, kafka

--- kafka/kafka-send/src/main/java/org/asname/integration/kafka/kafkasend/KafkaConfig.java
+++ kafka/kafka-send/src/main/java/org/asname/integration/kafka/kafkasend/KafkaConfig.java
@@ -1,14 +1,11 @@
 package org.asname.integration.kafka.kafkasend;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
 
 import java.util.HashMap;
@@ -18,41 +15,6 @@ import java.util.Map;
 @EnableKafka
 @ComponentScan(basePackages = "org.asname.integration.kafka.kafkasend")
 public class KafkaConfig {
-
-    @Bean
-    ConcurrentKafkaListenerContainerFactory<String, String>
-    kafkaListenerContainerFactory(ConsumerFactory<String, String> consumerFactory) {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory);
-        return factory;
-    }
-
-    @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(consumerProps());
-    }
-
-    private Map<String, Object> consumerProps() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        // ...
-        return props;
-    }
-
-/*    @Bean
-    public Sender sender(KafkaTemplate<String, String> template) {
-        return new Sender(template);
-    }*/
-
-/*    @Bean
-    public Listener listener() {
-        return new Listener();
-    }*/
 
     @Bean
     public ProducerFactory<String, String> producerFactory() {
